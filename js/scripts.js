92ed32d3c28c88913dc98ce97c4a82a34f195d6e
Rico Rodriguez <97664519+The-Rico@users.noreply.github.com>
2022-03-11 02:03:17 +0900

    General updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,52 @@
-//Module for gameBoard
-const gameBoard = (() => {
-    var parsed = "";
-    let gameBoard = 
-[{firstMove: "x"}, 
-{secondMove: "o"}, 
-{thirdMove: "x"}, 
-{fourthMove: "o"}, 
-{fifthMove: "x"}, 
-{sixthMove: "o"}, 
-{seventhMove: "x"}, 
-{eighthMove: "o"}, 
-{ninthMove: "x"}];
-    for (i = 0; i< gameBoard.length; i++) {
-        var myobj=  gameBoard[i];
-        for (var property in myobj) {
-            parsed += property + ": " + myobj[property] + "\n";          
+class Player {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+const Player1 = new Player('Player');
+
+let circle = "o"
+let x = "x"
+let num = 1
+function getRandomInt() {
+    num ++;
+}
+$(document).ready(function () {
+    $('td').each(function () {
+        $(this).attr('data-cell', getRandomInt());
+    });
+});
+let cellNumbers = [];
+    function generateCellNumber() {
+        for (var i = 0; i < 10; i++) {
+            cellNumvar = num++;
+            cellNumbers.push(cellNumvar);
         }
     }
-return {
-    gameBoard
-}
-  })();
-gameBoard
-//Factory for player
-const Player = (name, level) => {
-    const getLevel = () => level;
-    const getName  = () => name;
+    generateCellNumber();
+    $(document).ready(function () {
+        $('td').each(function (index) {
+            $(this).attr('data-cell', cellNumbers[index]);
+        })
+        
+        $('td').on('click', function(){
+            console.log($(this).attr('data-cell'));
+            $(this).attr('data-cell')
+        })
+    });
 
-    return {
-        };
-  };
+function playerMark() {
+    if (Player1 = true)
+    playerMark = "x";
+}
+function changeBoard() {
+if ($(this).attr('data-cell') == "" && $(this).attr('data-cell') != circle)
+console.log("not empty and not circle")
+ b$(this).html("X");
+
+
+if ($(this).attr('data-cell') == "" && $(this).attr('data-cell') != x)
+console.log("not empty and not circle")
+$(this).html("X");
+
+}
