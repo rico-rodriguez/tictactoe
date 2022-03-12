dd84c0b385d5f7839d9c73a75f18d40d9600eb5c
Rico Rodriguez <97664519+The-Rico@users.noreply.github.com>
2022-03-12 20:52:56 +0900

    Final push for the moment. Small updates on formatting

--- js/scripts.js
+++ js/scripts.js
@@ -8,39 +8,38 @@ cell7 = document.getElementById('cell7');
 cell8 = document.getElementById('cell8');
 cell9 = document.getElementById('cell9');
 let turns = [];
-let test = 
-[
- , , ,
- , , , 
- , , ,
-]
+let test = [, , , , , , , , , ]
 class Player {
     constructor(name, active) {
-      this.name = name;
-      this.active = active;
+        this.name = name;
+        this.active = active;
     }
-  }
+}
 const Player1 = new Player('Player1', true);
 const Player2 = new Player('Player2', true);
 
 
 function playGame() {
-//click player1 button
+    gameOver();
+    //click player1 button
     player1Button();
-//click player2 button
+    gameOver();
+    //click player2 button
     player2Button();
-//reset button
+    gameOver();
+    //reset button
     resetButton();
-//Clicking on each cell - playing game 
+    gameOver();
+    //Clicking on each cell - playing game 
     buttonClickLogic();
-    };
-
+    gameOver();
+};
 playGame();
 
-console.log(test)
 
 function player1Button() {
-    $('#player1').on('click', function(){
+    $('#player1').on('click', function () {
+        gameOver();
         Player2.active = false;
         Player1.active = true;
         gamePiece = x;
@@ -48,11 +47,13 @@ function player1Button() {
         $('#player2').addClass('disabled');
         $('#player1').addClass('disabled');
         console.log(turns)
+        
     })
 }
 
 function player2Button() {
-    $('#player2').on('click', function(){
+    $('#player2').on('click', function () {
+        gameOver();
         Player1.active = false;
         Player2.active = true;
         gamePiece = circle;
@@ -63,7 +64,7 @@ function player2Button() {
 }
 
 function resetButton() {
-    $('#reset').on('click', function(){
+    $('#reset').on('click', function () {
         gamePiece = null;
         $('#player2').removeClass('disabled');
         $('#player1').removeClass('disabled');
@@ -73,8 +74,8 @@ function resetButton() {
 }
 
 function buttonClickLogic() {
-    $('#cell1').click(function() {            
-        if(Player1.active == true & turns.length <= 9){
+    $('#cell1').click(function () {
+        if (Player1.active == true & turns.length <= 9 & $(this).html() == "") {
             ($(this).html(gamePiece));
             turns.push("X");
             Player1.active = false;
@@ -83,193 +84,210 @@ function buttonClickLogic() {
             $('#player2').removeClass('disabled');
             test[0] = "X"
             gamePiece = null;
-    
-        }else if(Player2.active == true & turns.length <= 9){
+
+        } else if (Player2.active == true & turns.length <= 9 & $(this).html() == "") {
             ($(this).html(gamePiece));
             turns.push("X");
             Player2.active = false;
             Player1.active = true;
             $('#player2').addClass('disabled');
             $('#player1').removeClass('disabled');
-             test[0] = "O"
+            test[0] = "O"
             gamePiece = null;
-       }
+        }
     });
-    $('#cell2').click(function() {            
-    if(Player1.active == true & turns.length <= 9){
-        ($(this).html(gamePiece));
-        turns.push("X");
-        Player1.active = false;
-        Player2.active = true;
-        $('#player1').addClass('disabled');
-        $('#player2').removeClass('disabled');
-        test[1] = "X"
-        gamePiece = null;
-    
-    }else if(Player2.active == true & turns.length <= 9){
-        ($(this).html(gamePiece));
-        turns.push("X");
-        Player2.active = false;
-        Player1.active = true;
-        $('#player2').addClass('disabled');
-        $('#player1').removeClass('disabled');
-        test[1] = "O"
-        gamePiece = null;
-    }
+    $('#cell2').click(function () {
+        if (Player1.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player1.active = false;
+            Player2.active = true;
+            $('#player1').addClass('disabled');
+            $('#player2').removeClass('disabled');
+            test[1] = "X"
+            gamePiece = null;
+
+        } else if (Player2.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player2.active = false;
+            Player1.active = true;
+            $('#player2').addClass('disabled');
+            $('#player1').removeClass('disabled');
+            test[1] = "O"
+            gamePiece = null;
+        }
     });
-    $('#cell3').click(function() {            
-    if(Player1.active == true & turns.length <= 9){
-    ($(this).html(gamePiece));
-    turns.push("X");
-    Player1.active = false;
-    Player2.active = true;
-    $('#player1').addClass('disabled');
-    $('#player2').removeClass('disabled');
-    test[2] = "X"
-    gamePiece = null;
-    
-    }else if(Player2.active == true & turns.length <= 9){
-    ($(this).html(gamePiece));
-    turns.push("X");
-    Player2.active = false;
-    Player1.active = true;
-    $('#player2').addClass('disabled');
-    $('#player1').removeClass('disabled');
-    test[2] = "O"
-    gamePiece = null;
-    } 
+    $('#cell3').click(function () {
+        if (Player1.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player1.active = false;
+            Player2.active = true;
+            $('#player1').addClass('disabled');
+            $('#player2').removeClass('disabled');
+            test[2] = "X"
+            gamePiece = null;
+
+        } else if (Player2.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player2.active = false;
+            Player1.active = true;
+            $('#player2').addClass('disabled');
+            $('#player1').removeClass('disabled');
+            test[2] = "O"
+            gamePiece = null;
+        }
     });
-    $('#cell4').click(function() {            
-    if(Player1.active == true & turns.length <= 9){
-    ($(this).html(gamePiece));
-    turns.push("X");
-    Player1.active = false;
-    Player2.active = true;
-    $('#player1').addClass('disabled');
-    $('#player2').removeClass('disabled');
-    test[3] = "X"
-    gamePiece = null;
-    
-    }else if(Player2.active == true & turns.length <= 9){
-    ($(this).html(gamePiece));
-    turns.push("X");
-    Player2.active = false;
-    Player1.active = true;
-    $('#player2').addClass('disabled');
-    $('#player1').removeClass('disabled');
-    test[3] = "O"
-    gamePiece = null;
-    } 
+    $('#cell4').click(function () {
+        if (Player1.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player1.active = false;
+            Player2.active = true;
+            $('#player1').addClass('disabled');
+            $('#player2').removeClass('disabled');
+            test[3] = "X"
+            gamePiece = null;
+
+        } else if (Player2.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player2.active = false;
+            Player1.active = true;
+            $('#player2').addClass('disabled');
+            $('#player1').removeClass('disabled');
+            test[3] = "O"
+            gamePiece = null;
+        }
     });
-    $('#cell5').click(function() {            
-    if(Player1.active == true & turns.length <= 9){
-    ($(this).html(gamePiece));
-    turns.push("X");
-    Player1.active = false;
-    Player2.active = true;
-    $('#player1').addClass('disabled');
-    $('#player2').removeClass('disabled');
-    test[4] = "X"
-    gamePiece = null;
-    
-    }else if(Player2.active == true & turns.length <= 9){
-    ($(this).html(gamePiece));
-    turns.push("X");
-    Player2.active = false;
-    Player1.active = true;
-    $('#player2').addClass('disabled');
-    $('#player1').removeClass('disabled');
-    test[4] = "O"
-    gamePiece = null;
-    } 
+    $('#cell5').click(function () {
+        if (Player1.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player1.active = false;
+            Player2.active = true;
+            $('#player1').addClass('disabled');
+            $('#player2').removeClass('disabled');
+            test[4] = "X"
+            gamePiece = null;
+
+        } else if (Player2.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player2.active = false;
+            Player1.active = true;
+            $('#player2').addClass('disabled');
+            $('#player1').removeClass('disabled');
+            test[4] = "O"
+            gamePiece = null;
+        }
     });
-    $('#cell6').click(function() {            
-    if(Player1.active == true & turns.length <= 9){
-    ($(this).html(gamePiece));
-    turns.push("X");
-    Player1.active = false;
-    Player2.active = true;
-    $('#player1').addClass('disabled');
-    $('#player2').removeClass('disabled');
-    test[5] = "X"
-    gamePiece = null;
-    
-    }else if(Player2.active == true & turns.length <= 9){
-    ($(this).html(gamePiece));
-    turns.push("X");
-    Player2.active = false;
-    Player1.active = true;
-    $('#player2').addClass('disabled');
-    $('#player1').removeClass('disabled');
-    test[5] = "O"
-    gamePiece = null;
-    } 
+    $('#cell6').click(function () {
+        if (Player1.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player1.active = false;
+            Player2.active = true;
+            $('#player1').addClass('disabled');
+            $('#player2').removeClass('disabled');
+            test[5] = "X"
+            gamePiece = null;
+
+        } else if (Player2.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player2.active = false;
+            Player1.active = true;
+            $('#player2').addClass('disabled');
+            $('#player1').removeClass('disabled');
+            test[5] = "O"
+            gamePiece = null;
+        }
     });
-    $('#cell7').click(function() {            
-    if(Player1.active == true & turns.length <= 9){
-    ($(this).html(gamePiece));
-    turns.push("X");
-    Player1.active = false;
-    Player2.active = true;
-    $('#player1').addClass('disabled');
-    $('#player2').removeClass('disabled');
-    test[6] = "X"
-    gamePiece = null;
-    
-    }else if(Player2.active == true & turns.length <= 9){
-    ($(this).html(gamePiece));
-    turns.push("X");
-    Player2.active = false;
-    Player1.active = true;
-    $('#player2').addClass('disabled');
-    $('#player1').removeClass('disabled');
-    test[6] = "O"
-    gamePiece = null;
-    } 
+    $('#cell7').click(function () {
+        if (Player1.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player1.active = false;
+            Player2.active = true;
+            $('#player1').addClass('disabled');
+            $('#player2').removeClass('disabled');
+            test[6] = "X"
+            gamePiece = null;
+
+        } else if (Player2.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player2.active = false;
+            Player1.active = true;
+            $('#player2').addClass('disabled');
+            $('#player1').removeClass('disabled');
+            test[6] = "O"
+            gamePiece = null;
+        }
     });
-    $('#cell8').click(function() {            
-    if(Player1.active == true & turns.length <= 9){
-    ($(this).html(gamePiece));
-    turns.push("X");
-    Player1.active = false;
-    Player2.active = true;
-    $('#player1').addClass('disabled');
-    $('#player2').removeClass('disabled');
-    test[7] = "X"
-    gamePiece = null;
-    
-    }else if(Player2.active == true & turns.length <= 9){
-    ($(this).html(gamePiece));
-    turns.push("X");
-    Player2.active = false;
-    Player1.active = true;
-    $('#player2').addClass('disabled');
-    $('#player1').removeClass('disabled');
-    test[7] = "O"
-    gamePiece = null;
-    } 
+    $('#cell8').click(function () {
+        if (Player1.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player1.active = false;
+            Player2.active = true;
+            $('#player1').addClass('disabled');
+            $('#player2').removeClass('disabled');
+            test[7] = "X"
+            gamePiece = null;
+
+        } else if (Player2.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player2.active = false;
+            Player1.active = true;
+            $('#player2').addClass('disabled');
+            $('#player1').removeClass('disabled');
+            test[7] = "O"
+            gamePiece = null;
+        }
     });
-    $('#cell9').click(function() {            
-    if(Player1.active == true & turns.length <= 9){
-    ($(this).html(gamePiece));
-    turns.push("X");
-    Player1.active = false;
-    Player2.active = true;
-    $('#player1').addClass('disabled');
-    $('#player2').removeClass('disabled');
-    test[8] = "X"
-    gamePiece = null;
-    
-    }else if(Player2.active == true & turns.length <= 9){
-    ($(this).html(gamePiece));
-    turns.push("X");
-    Player2.active = false;
-    Player1.active = true;
-    $('#player2').addClass('disabled');
-    $('#player1').removeClass('disabled');
-    test[8] = "O"
-    gamePiece = null;
-    } 
+    $('#cell9').click(function () {
+        if (Player1.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player1.active = false;
+            Player2.active = true;
+            $('#player1').addClass('disabled');
+            $('#player2').removeClass('disabled');
+            test[8] = "X"
+            gamePiece = null;
+
+        } else if (Player2.active == true & turns.length <= 9 & $(this).html() == "") {
+            ($(this).html(gamePiece));
+            turns.push("X");
+            Player2.active = false;
+            Player1.active = true;
+            $('#player2').addClass('disabled');
+            $('#player1').removeClass('disabled');
+            test[8] = "O"
+            gamePiece = null;
+        }
     });
     //END OF BUTTON CLICK LOGIC
+}
+
+const winningSolutions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+function gameOver() {
+    if (test.includes == "X") {
+        console.log("game over");
     }
+}
+gameOver();
